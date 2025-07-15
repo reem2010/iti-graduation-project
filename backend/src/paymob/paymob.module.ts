e111bc12c0ba06aefe8e89b202b3df79fdb53d36
reem <tarekreem950@gmail.com>
2025-07-15 16:19:46 +0300

    update webhook

--- backend/src/paymob/paymob.module.ts
+++ backend/src/paymob/paymob.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { PaymobService } from './paymob.service';
+import { TransactionModule } from 'src/transaction/transaction.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, TransactionModule],
   providers: [PaymobService],
   exports: [PaymobService],
 })
