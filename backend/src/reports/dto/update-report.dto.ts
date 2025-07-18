280391f3784e50d2106057ffd0923f66978a14c2
reem <tarekreem950@gmail.com>
2025-07-19 00:02:13 +0300

    reports

--- backend/src/reports/dto/update-report.dto.ts
+++ backend/src/reports/dto/update-report.dto.ts
@@ -1,4 +1,27 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReportDto } from './create-report.dto';
+// src/reports/dto/update-report.dto.ts
+import { IsOptional, IsString, IsObject } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
-export class UpdateReportDto extends PartialType(CreateReportDto) {}
+export class UpdateReportDto {
+  @ApiPropertyOptional({ example: 'Flu' })
+  @IsOptional()
+  @IsString()
+  diagnosis?: string;
+
+  @ApiPropertyOptional({ example: 'Rest and drink fluids' })
+  @IsOptional()
+  @IsString()
+  prescription?: string;
+
+  @ApiPropertyOptional({ example: 'Patient to follow up in 2 weeks' })
+  @IsOptional()
+  @IsString()
+  notes?: string;
+
+  @ApiPropertyOptional({
+    example: { reportUrl: 'https://link.com/report.pdf' },
+  })
+  @IsOptional()
+  @IsObject()
+  documents?: object;
+}
--- backend/src/reports/reports.controller.ts
+++ backend/src/reports/reports.controller.ts
@@ -1,34 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Patch,
+  Query,
+  Body,
+  Request,
+  UseGuards,
+  Get,
+} from '@nestjs/common';
 import { ReportsService } from './reports.service';
-import { CreateReportDto } from './dto/create-report.dto';
 import { UpdateReportDto } from './dto/update-report.dto';
+import {
+  SwaggerTags,
+  SwaggerAuth,
+  SwaggerUpdateReport,
+  SwaggerGetPatientsReports,
+} from './reports.swagger';
+import { Roles } from 'src/auth/roles.decorator';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
 
 @Controller('reports')
+@SwaggerTags()
+@SwaggerAuth()
+@UseGuards(JwtAuthGuard)
+@Roles('doctor')
 export class ReportsController {
-  constructor(private readonly reportsService: ReportsService) {}
+  constructor(private reportsService: ReportsService) {}
 
-  @Post()
-  create(@Body() createReportDto: CreateReportDto) {
-    return this.reportsService.create(createReportDto);
+  @Patch()
+  @SwaggerUpdateReport()
+  async updateAppointmentReport(
+    @Request() req: any,
+    @Query('appointmentId') appointmentId: string,
+    @Body() dto: UpdateReportDto,
+  ) {
+    const doctorId = req.user.userId;
+    return this.reportsService.updateAppointmentReport(
+      doctorId,
+      Number(appointmentId),
+      dto,
+    );
   }
 
-  @Get()
-  findAll() {
-    return this.reportsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reportsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateReportDto: UpdateReportDto) {
-    return this.reportsService.update(+id, updateReportDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reportsService.remove(+id);
+  @Get('')
+  @SwaggerGetPatientsReports()
+  async getDoctorPatientsReports(@Request() req: any) {
+    const doctorId = req.user.userId;
+    return this.reportsService.getDoctorPatientsReports(doctorId);
   }
 }
